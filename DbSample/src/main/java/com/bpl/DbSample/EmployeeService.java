075a0efb26a87ff73e835a07ffc91aeac6a75aa3
bharath <dongisharapu999@gmail.com>
2020-08-02 00:28:57 +0530

    small gap changes from intellij

--- DbSample/src/main/java/com/bpl/DbSample/EmployeeService.java
+++ DbSample/src/main/java/com/bpl/DbSample/EmployeeService.java
@@ -17,68 +17,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class EmployeeService {
 
-	@GetMapping(path = "/emp", produces = { "application/xml", "application/json" ,"text/plain"})
-	public List<Employee> findALl() throws SQLException {
+    @GetMapping(path = "/emp", produces = {"application/xml", "application/json", "text/plain"})
+    public List<Employee> findALl() throws SQLException {
 
-		ArrayList<Employee> arrayList = new ArrayList<Employee>();
-		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
-		PreparedStatement prepareStatement = connection.prepareStatement("select * from emp_tbl");
-		ResultSet resultSet = prepareStatement.executeQuery();
-		while (resultSet.next()) {
-			Employee employee = new Employee();
-			employee.setEno(resultSet.getInt("eno"));
-			employee.setName(resultSet.getString("name"));
-			arrayList.add(employee);
-		}
+        ArrayList<Employee> arrayList = new ArrayList<Employee>();
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
+        PreparedStatement prepareStatement = connection.prepareStatement("select * from emp_tbl");
+        ResultSet resultSet = prepareStatement.executeQuery();
+        while (resultSet.next()) {
+            Employee employee = new Employee();
+            employee.setEno(resultSet.getInt("eno"));
+            employee.setName(resultSet.getString("name"));
+            arrayList.add(employee);
+        }
 
-		return arrayList;
-	}
-	
-	
-	
-	
-	
+        return arrayList;
+    }
 
-	@PostMapping(path = "/emp1", produces = { "application/xml", "application/json" ,"text/plain"}, consumes = { "application/xml",
-			"application/json","text/plain" })
-	public int insert(Employee employee) throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
-		PreparedStatement prepareStatement = connection.prepareStatement("insert into emp_tbl values(?,?,?,?)");
-		prepareStatement.setInt(1, 124);
-		prepareStatement.setString(2, "ajay");
-		prepareStatement.setDouble(3, 500d);
-		prepareStatement.setString(4, "dept");
-		int update = prepareStatement.executeUpdate();
+    @PostMapping(path = "/emp1", produces = {"application/xml", "application/json", "text/plain"}, consumes = {"application/xml",
+            "application/json", "text/plain"})
+    public int insert(Employee employee) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
+        PreparedStatement prepareStatement = connection.prepareStatement("insert into emp_tbl values(?,?,?,?)");
+        prepareStatement.setInt(1, 124);
+        prepareStatement.setString(2, "ajay");
+        prepareStatement.setDouble(3, 500d);
+        prepareStatement.setString(4, "dept");
+        int update = prepareStatement.executeUpdate();
 
-		return update;
+        return update;
 
-	}
+    }
 
-	
+    @PostMapping(path = "/emp2", produces = {"application/xml", "application/json", "text/plain"}, consumes = {"application/xml",
+            "application/json", "text/plain"})
+    public int update(Employee employee) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
+        PreparedStatement prepareStatement = connection.prepareStatement("update emp_tbl set name= 'bharah' where eno=1");
+        int update = prepareStatement.executeUpdate();
+        return update;
 
-	@PostMapping(path = "/emp2", produces = { "application/xml", "application/json","text/plain" }, consumes = { "application/xml",
-			"application/json","text/plain" })
-	public int update(Employee employee) throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
-		PreparedStatement prepareStatement = connection.prepareStatement("update emp_tbl set name= 'bharah' where eno=1");
-		int update = prepareStatement.executeUpdate();
-		return update;
-
-	}
+    }
 
 
-	@DeleteMapping(path = "/emp3", produces = { "application/xml", "application/json","text/plain" }, consumes = { "application/xml",
-			"application/json","text/plain" })
-	public int delete(Employee employee) throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
-		PreparedStatement prepareStatement = connection.prepareStatement("delete from emp_tbl where eno = 2");
-		System.out.println(employee);
-		int update = prepareStatement.executeUpdate();
+    @DeleteMapping(path = "/emp3", produces = {"application/xml", "application/json", "text/plain"}, consumes = {"application/xml",
+            "application/json", "text/plain"})
+    public int delete(Employee employee) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "bms", "abc");
+        PreparedStatement prepareStatement = connection.prepareStatement("delete from emp_tbl where eno = 2");
+        System.out.println(employee);
+        int update = prepareStatement.executeUpdate();
 
-		return update;
+        return update;
 
-	}
+    }
 
-	
-	
+
 }
